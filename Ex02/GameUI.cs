0b865508821d00dba406844b050acc16f5d327ec
liadT22 <liadt22@gmail.com>
2022-12-21 16:13:15 +0200

    Add project files.

---
+++ Ex02/GameUI.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Ex02
+{
+    internal class GameUI
+    {
+        public static void PrintBox(eBoxStatuses[,] i_BoxStatusMatrix, int i_BoardSize)
+        {
+            char topBoardLetter = 'A';
+            Console.Write("    ");
+            for (int i = 0; i <= i_BoardSize; i++)
+            {
+                for (int k = 0; k < i_BoardSize; k++)
+                {
+                    if (i == 0)
+                    {
+                        Console.Write(format: "  {0} ", topBoardLetter);
+                        topBoardLetter++;
+                    }
+                    else if (k == 0)
+                    {
+                        Console.Write(format: " {0}  |", i);
+                        printBoxStatus(i_BoxStatusMatrix[i - 1, k]);
+                    }
+                    else
+                    {
+                        printBoxStatus(i_BoxStatusMatrix[i - 1, k]);
+                    }
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("    =================================");
+            }
+        }
+
+        private static void printBoxStatus(eBoxStatuses status)
+        {
+            switch (status)
+            {
+                case eBoxStatuses.Natural:
+                    Console.Write("   |");
+                    break;
+                case eBoxStatuses.PlayerOne:
+                    Console.Write(" X |");
+                    break;
+                case eBoxStatuses.PlayerTwo:
+                    Console.Write(" O |");
+                    break;
+            }
+        }
+    }
+}
---
+++ Ex02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex02
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            int number = 8;
+            eBoxStatuses[,] mBoxStatusMatrix = new eBoxStatuses[number, number];
+            mBoxStatusMatrix[0, 0] = eBoxStatuses.PlayerOne;
+            mBoxStatusMatrix[0, 7] = eBoxStatuses.PlayerOne;
+            mBoxStatusMatrix[7, 0] = eBoxStatuses.PlayerOne;
+            mBoxStatusMatrix[7, 7] = eBoxStatuses.PlayerOne;
+            mBoxStatusMatrix[3, 3] = eBoxStatuses.PlayerOne;
+            mBoxStatusMatrix[4, 4] = eBoxStatuses.PlayerOne;
+            mBoxStatusMatrix[4, 3] = eBoxStatuses.PlayerTwo;
+            mBoxStatusMatrix[3, 4] = eBoxStatuses.PlayerTwo;
+            GameUI.PrintBox(mBoxStatusMatrix, 8);
+            Console.ReadLine();
+        }
+    }
+
+    public enum eBoxStatuses
+    {
+        Natural,
+        PlayerOne,
+        PlayerTwo
+    }
+}
